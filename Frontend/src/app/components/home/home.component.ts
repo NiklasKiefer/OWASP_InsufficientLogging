fa9c6c4dcdea4c066fa2b4128f9b7b87e4a664ac
Chavalentas <stefanchvala@gmail.com>
2022-04-22 19:55:22 +0200

    Changed frontend, modified logout to support logging in the backend

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggerService } from '../../services/logger.service';
+import apiConfiguration from '../../../assets/config/api-config.json';
+import { HttpClient } from '@angular/common/http';
 import { Router} from '@angular/router';
 
 @Component({
@@ -9,12 +11,26 @@ import { Router} from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private loggerService: LoggerService, private router: Router) { }
+  constructor(private loggerService: LoggerService, private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  public logout(): void {
-    this.router.navigateByUrl('');
+  public logoutUser(): void {
+    let url = apiConfiguration.logout + history.state.username + '&' + history.state.password;
+    
+    this.httpClient
+    .post<any>(url, {})
+    .subscribe({
+      next: response => {
+        if (response.created) {
+          this.loggerService.info('User logged out.');
+        }
+    },
+    complete: () => {
+      this.loggerService.info('Logging out complete.');
+      this.router.navigateByUrl('');
+    },
+    });
   }
 }
